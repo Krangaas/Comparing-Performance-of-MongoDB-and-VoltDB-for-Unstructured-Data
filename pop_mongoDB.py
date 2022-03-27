2adbff6e9c2b06da0c29717026ba2240e1c660b4
Mcmaistro <abr043@uit.com>
2022-03-27 16:22:24 +0200

    with timer

--- pop_mongoDB.py
+++ pop_mongoDB.py
@@ -5,6 +5,7 @@ import os
 import io
 import matplotlib.pyplot as plt
 from PIL import Image
+import time
 
 
 N_FILES = int(sys.argv[1])
@@ -16,6 +17,7 @@ def insert():
     path = './catfolder/'
 
     PROCESSED_FILES = 0
+    t1 = time.time()
     for filename in os.listdir(path):
         if filename.endswith("jpg") and PROCESSED_FILES < N_FILES:
             file = path + filename
@@ -24,19 +26,20 @@ def insert():
                 image = {"filename":filename, 'images':contents}
                 images.insert_one(image).inserted_id
                 PROCESSED_FILES += 1
+    t2 = time.time()
+    tot_time = t2 - t1
+    print(tot_time)
 
 def select():
     connection = MongoClient("localhost", 27017)
     db = connection['CAT']
     images = db.images
-    response = images.find_one()
-    print(response)
-    pil_image = Image.open(io.BytesIO(response["images"]))
-    plt.imshow(pil_image)
-    plt.show()
+    t1 = time.time()
+    response = images.find()
+    t2 = time.time()
+    tot_time = t2 - t1
+    print(tot_time)
 
-    #for x in response:
-    #    print(x)
 
 
 def plot_img(data):
@@ -47,5 +50,5 @@ def plot_img(data):
 
 
 if __name__=='__main__':
-    #insert()
-    select()
+    insert()
+    #select()
